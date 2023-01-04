8595aa4629e93f1240b104e1ed025386aef5ef30
AvinashDRao <121382514+AvinashDRao@users.noreply.github.com>
2023-01-04 10:09:37 +0530

    Update OOPS (CLASSES).py

--- OOPS (CLASSES).py
+++ OOPS (CLASSES).py
@@ -52,41 +52,42 @@ print("employee2 age is: ",employee2.age)
 # Class Customer = Request book, Return book
 
 class Library:
-        
-        def __init__(self,list_books):
-            self.avilable_books=list_books
-            
-        def display_available_books(self):
-            print() 
-            print("Available books in the library are ")
-            for book in self.avilable_books:
-                print(book)
-            
-        def lend_books(self,requested_book):
-            if requested_book in self.available_book:
-                self.available_books.remove(requeted_book)
-                print("Thank you for lending the book")
-            else:
-                print("Sorry the requested book is not available")
-            
-        def add_books(self,return_book):
-            self.available_books.append(return_book)
-            print("Thank you for returning the book !!!")
-            
-        
-class Customer:
-    
-        def request_books(self):
-            self.requested_book=input("Please enter the book you want: ")
-            return self.requested_book
 
-        def return_books(self):
-            print("Enter the name of the book you wanted to return: ")
-            return_book=input()
-            return return_book
-      
-library = Library(["think a like a monk","think and grow rich","2 states"])
-customer = Customer
+    def __init__(self, list_books):
+        self.available_books = list_books
+
+    def display_available_books(self):
+        print()
+        print("Available books in the library are ")
+        for book in self.available_books:
+            print(book)
+
+    def lend_book(self, requested_book):
+        if requested_book in self.available_books:
+            self.available_books.remove(requested_book)
+            print("Thank you for lending the book")
+        else:
+            print("Sorry the requested book is not available")
+
+    def add_book(self, return_book):
+        self.available_books.append(return_book)
+        print("Thank you for returning the book !!!")
+
+
+class Customer:
+
+    def request_book(self):
+        self.requested_book = input("Please enter the book you want: ")
+        return self.requested_book
+
+    def return_book(self):
+        print("Enter the name of the book you wanted to return: ")
+        return_book = input()
+        return return_book
+
+
+library = Library(["think like a monk", "think and grow rich", "2 states"])
+customer = Customer()
 
 while True:
     print("Enter 1 to display the available books ")
@@ -94,16 +95,18 @@ while True:
     print("Enter 3 for returning the books ")
     print("Enter 4 to exit ")
     user_choice = int(input("Enter your choice: "))
-    if user_chioce == 1:
+    if user_choice == 1:
         library.display_available_books()
-    elif user_chioce == 2:
-        requested_book=customer.request_book()
-        library.lend_books(requested_book)
-    elif user_chioce == 3:
-        return_book=customer.retun_book()
-        library.add_books(return_book)
-    elif user_chioce == 4:
+    elif user_choice == 2:
+        requested_book = customer.request_book()
+        library.lend_book(requested_book)
+    elif user_choice == 3:
+        return_books = customer.return_book()
+        library.add_book(return_books)
+    elif user_choice == 4:
         quit()
     else:
         print("please enter appropriate value ")
-
+        
+        
+        
